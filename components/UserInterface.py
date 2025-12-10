c65b52f0a633539dabf3851e4e610ba29b80eb01
Alexander Ayer <alexma@host-0800-865f09e9424c.pool.net.maine.edu>
2025-12-10 16:39:44 -0500

    Made an interface for the UI

--- components/UserInterface.py
+++ components/UserInterface.py
@@ -1,1 +1,22 @@
 # This is the UI
+
+import tkinter as TK
+
+def create_instrument_page():
+    print("")
+
+def create_setup_page():
+    print("")
+
+def create_advanced_settings():
+    print("")
+
+def create_take_sample_popup():
+    print("")
+
+def create_sample_taken_popup():
+    print("")
+
+def create_resetting_popup():
+    print("")
+
