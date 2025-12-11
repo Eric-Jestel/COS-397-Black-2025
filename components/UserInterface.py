47e7609f25d47bd122a9b1ff2def75f31fa94e96
Bryce Roy <bryce.roy011203@gmail.com>
2025-12-11 18:45:28 -0500

    Test

--- components/UserInterface.py
+++ components/UserInterface.py
@@ -1,17 +1,21 @@
 # This is the UI
 
 import tkinter as TK
-
-def create_instrument_page():
-    print("")
+from tkinter import *
 
 def create_setup_page():
+    #root
+    #buttons call other functions
+    print("")
+
+def create_instrument_page():
     print("")
 
 def create_advanced_settings():
     print("")
 
 def create_take_sample_popup():
+
     print("")
 
 def create_sample_taken_popup():
