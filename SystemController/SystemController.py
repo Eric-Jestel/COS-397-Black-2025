f63d09e16aa4fdcc0d9ad35614bda378ca5c9294
Bryce Roy <bryce.roy011203@gmail.com>
2026-02-02 14:03:24 -0500

    Adding everything and trying to pull origin

--- SystemController/SystemController.py
+++ SystemController/SystemController.py
@@ -1,34 +1,51 @@
 # This is the system controller
-
+import User_Interface
 
 #Need necessary functions such as run test
 
-def clicked(function):
+def clicked(function, endProgram): #IN PROGRESS
     match function:
         #runs machine
         case "runLabMachine":
             parameters = ""
             runLabMachine(parameters)
-            #should return the JSON file
-            #need to send the JSON file to 
+            #while running
+            #should get running data
+            data = getRunData()
+            #need to send the JSON file to UI -- Need to get variable from Alex
+        
         #stops the checks and shuts down the program
         case "stopProgram":
             parameters = ""
-            stopProgram(parameters)
+            endProgram = stopProgram(parameters)
         #Does nothing because nothing was pressed
         case "":
-            return
+            return False
         case _:
-            return print("Please enter a valid instruction")
+            print("Please enter a valid instruction")
+            return False
 
-def runLabMachine(parameters):
+def runLabMachine(parameters): #NOT DONE
     parameters
     #sends instructions to machine to run test
     return
 
-def stopProgram(parameters):
-    parameters
+def getRunData(): #NOT DONE DO ONE THING AT A TIME DAMNIT. figure out additional needs later...
+    with open("ExampleData.txt") as data:
+        lineData = data.readlines()
+    return lineData
+
+def sendData():
+    #needs to be able to send the data to the server controller
+    data = 0 #need to put in JSON file from IC
+    #Need to send data to server controller
+    return
+
+def stopProgram(parameters): #NOT DONE
     #stops the program and closes window
+    #tells server controller to disconnect
+    #tells UI to shutdown
+    parameters
     return True
 
 #main function
@@ -37,6 +54,6 @@ def main():
     function = ""
     while endProgram != True:
         #checks for what is pressed
-        clicked(function)
+        endProgram = clicked(function)
     return 
 main()
