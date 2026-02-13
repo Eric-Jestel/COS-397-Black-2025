48d1fe57ecad74b0a3a7d6822036b2ae8d775b67
Linkaboy <bryce.roy011203@gmail.com>
2026-02-12 21:19:43 -0500

    Finished up the System controller according to current server and instrument controllers. I also added in some error codes in the form of 3 digits to help diagnose the issue. I am not sure if the UI will be handling reconnection and what not, but that can be discussed at the next meeting.

--- components/SystemController/SystemController.py
+++ components/SystemController/SystemController.py
@@ -3,136 +3,117 @@
 #from User_Interface import main.py ?????
 from InstrumentController import InstrumentController
 from ServerController import ServerController
-#Need necessary functions such as run test
-def clicked(function, ServController, InstController): #IN PROGRESS
-    match function:
-#------------------------------------------------------------------------------------------------------------------------------------------
-        case "startUp":
-            startUp(ServController, InstController)
-            return False
-#------------------------------------------------------------------------------------------------------------------------------------------
-        case "signIn":
-            signIn(ServController)
-            return False
-#------------------------------------------------------------------------------------------------------------------------------------------
-        case "signOut":
-            signOut(ServController)
-            return False
-#------------------------------------------------------------------------------------------------------------------------------------------
-        case "takeBlank":
-            #verify the InstrumentController is connected
-            #no function yet
-            #take blank
-            Blank = InstController.take_blank("Test.txt")
-            #send blank to UI -- Need variable from Alex!
-            return False
-#------------------------------------------------------------------------------------------------------------------------------------------
-        case "setBlank":
-            #this dosn't really need its own function since it is defined in the Instrument Controller
-            InstController.set_blank("Test.txt")
-            return False
-#------------------------------------------------------------------------------------------------------------------------------------------
-        case "runLabMachine": 
-            #runs the lab machine
-            #while running it should get running data
-            #need to send the text file to UI -- Need to get variable from Alex
-            #Once data is fully processed send the JSON file to the Server Controller to be sent to the InterChemNet
-            if ServController.is_logged_in():
-                ServController.send_data("Test")
-            print("not stopped")
-            return False
-#------------------------------------------------------------------------------------------------------------------------------------------
-        case "stopProgram":
-            stopProgram(ServController, InstController)
-            return True
-#------------------------------------------------------------------------------------------------------------------------------------------
-        case "":
-            #do nothing :D
-            print("not stopped")
-            return False
-#------------------------------------------------------------------------------------------------------------------------------------------
-        case _: #NOT DONE
-            print("Please enter a valid argument. Function hard coded string does not have an accepted string")
-            return False
 #------------------------------------------------------------------------------------------------------------------------------------------
 class SystemController:
     #I need variables
-
+    ServController = ServerController()
+    InstController = InstrumentController()
     #------------------------------------------------------------------------------------------------------------------------------------------
-    def startUp(self, ServController, InstController):
-        #Loop to make sure startup is finished before moving on
-        startUpComplete = False
-        while startUpComplete == False:
-            #verify machine connection
-            if InstController.setup():
-                #verify server connection
-                if ServController.connect():
-                    print("Connected")
-                    startUpComplete = True
-                else:
-                    print("Please make sure the device is connected to the internet and the interchem net is operational")
-                    useless = input("Press 'Enter' to continue: ")
+    def startUp(self):
+        #verify machine connection
+        InstConn = self.InstController.setup()
+        if InstConn == True:
+            #verify server connection
+            ServConn = self.ServController.connect()
+            if ServConn == True:
+                return 111
             else:
-                print("Please make sure the instrument is plugged in and turned on")
-                useless = input("Press 'Enter' to continue: ")
-        print("starting up")
-        return True
-    #------------------------------------------------------------------------------------------------------------------------------------------
-    def signIn(self, ServController, username):
-        #verify connection to ICN
-        if ServController.connect():
-            #send information to server controller to sign in
-            if ServController.login(username):
-                print(ServController.user + ": signing in")
-                return True
-            else:
-                print("Please enter valid credentials")
-                return False
+                return 110
         else:
-            print("Please make sure the device is connected to the internet and the interchem net is operational")
-            useless = input("Press 'Enter' to continue: ")
-            return False
+            return 100
     #------------------------------------------------------------------------------------------------------------------------------------------
-    def signOut(self, ServController):
+    def signIn(self, username):
+        #verify connection to ICN
+        if self.ServController.connect():
+            #send information to server controller to sign in
+            loggedIn=self.ServController.login(username)
+            if loggedIn==True:
+                return 222
+            else:
+                return 220
+        else:
+            return 110
+    #------------------------------------------------------------------------------------------------------------------------------------------
+    def signOut(self):
         #check to see if anyone is logged in already
-        if ServController.is_logged_in():
-            ServController.logout()
+        if self.ServController.is_logged_in():
+            if self.ServController.logout():
+                return 333
+            else:
+                return 330
+        else:
+            return 300
     #------------------------------------------------------------------------------------------------------------------------------------------
-
+    def runLabMachine(self):
+        #verify instrument connection
+        if self.InstController:
+            #sends instructions to machine to run test
+            data = self.InstController.take_sample()
+            if data:
+                #verify server connection
+                if self.ServController.connect():
+                    #sends data to UI somehow and send data to server controller to send to the ICN
+                    self.ServController.send_data(data)
+                    return 440, data
+                else:
+                    return 110
+            else:
+                return 400
+        else:
+            return 100
     #------------------------------------------------------------------------------------------------------------------------------------------
-    def runLabMachine(self, ServController, InstController):
-        #sends instructions to machine to run test
-        data = InstController.take_sample()
-        #sends data to UI somehow and send data to server controller to send to the ICN
-        ServController.send_data(data)
+    def takeBlank(self):
+        #verify instrument connection
+        if self.InstController:
+            #sends instructions to machine to run test
+            data = self.InstController.take_sample()
+            if data:
+                #send data to UI to hold onto for setting the blank
+                return 444, data
+            else:
+                return 400
+        else:
+            return 100
     #------------------------------------------------------------------------------------------------------------------------------------------
-    def stopProgram(self, ServController, InstController):
-        #sends instructions for the Server controller to disconnect
-        if ServController.is_logged_in():
-            ServController.logout()
-        #sends instructions for the Instrument Controller to shut down the machine
-        InstController.shutdown()
-        print("stopped")
+    def setBlank(self, data):
+        #verify instrument connection
+        if self.InstController:
+            #sends instructions to machine to run test
+            data = self.InstController.take_sample()
+            if data:
+                #send instructions to machine to set data
+                set = self.InstController.set_Blank(data)
+                if set == True:
+                    return 555
+                else:
+                    return 550
+            else:
+                return 400
+        else:
+            return 100
+    #------------------------------------------------------------------------------------------------------------------------------------------
+    def stopProgram(self):
+        #verify the server controller is connected and logged in
+        if self.ServController.connect():
+            if self.ServController.is_logged_in():
+                #sends instructions for the Server controller to disconnect
+                self.ServController.logout()
+            #verify instrument connection
+            if self.InstController:
+                #sends instructions for the Instrument Controller to shut down the machine
+                self.InstController.shutdown()
+                return 666
+            else:
+                return 100
+        else:
+            return 110
     #------------------------------------------------------------------------------------------------------------------------------------------
 #error code stuffs
 '''
-1) Machine is not connecting
-2) Server is not connecting
-3) No blank to set
-3) 
+1) 100 = Machine is not connecting
+2) 110 = Server is not connecting
+3) 220 = Not a valid Account
+4) 330 = User not logged in
+5) 400 = No data :(
+6) 550 = No blank to set
 '''
-
-#main function
-def main():
-    endProgram = False
-    function = ""
-    ServController = ServerController()
-    InstController = InstrumentController()
-    endProgram = clicked(function, ServController, InstController)
-    while endProgram != True:
-        #checks for what is pressed
-        function = input("Please enter:\n runLabMachine\n or stopProgram\n Or type nothing\n   Answer: ")
-        endProgram = clicked(function, ServController, InstController)
-        
-    return 
-main()
