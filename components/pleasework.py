fb9b50ced1bd451924c7388004bffe7d3569f761
CILet99 <letourneauc@mssm.org>
2026-03-01 07:15:37 -0500

    fixed linitng

--- components/pleasework.py
+++ components/pleasework.py
@@ -9,16 +9,20 @@ class InstrumentControllerOpus:
         # needs to connect to opus
         self.opus = Opus()
         self.blankPath = None
-    # ------------------------------------------------------------------------------------------------------------------------------------------
 
+    # ------------------------------------------------------------------------------------------------------------------------------------------
     def getBlank(self):
         # gets a reference sample aka a blank from the machine
         print("Taking Blank")
         self.opus.measure_ref()
         # TA can set name so fix later
-        self.opus.save_ref(r"C:\Users\Public\Documents\Bruker\Opus_8.8.4\Data\RefBlank.0")
+        self.opus.save_ref(
+            r"C:\Users\Public\Documents\Bruker\Opus_8.8.4\Data\RefBlank.0"
+        )
 
     # TA can change so fix later
-    def loadBlank(self, filepath=r"C:\Users\Public\Documents\Bruker\Opus_8.8.4\Data\RefBlank.0"):
+    def loadBlank(
+        self, filepath=r"C:\Users\Public\Documents\Bruker\Opus_8.8.4\Data\RefBlank.0"
+    ):
         # uses the filepath to load the blank
         self.opus.open(filepath)
